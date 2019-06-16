62841034660cd7f81279ba8d4c58d6ec35526d48
Chelsea <xuke1108@163.com>
2019-06-16 23:01:56 +0800

    V1.0.0.1

--- config/src/main/java/com/magic/ConfigApplication.java
+++ config/src/main/java/com/magic/ConfigApplication.java
@@ -1,5 +1,6 @@
 package com.magic;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -11,4 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringCloudApplication
 @EnableConfigServer
 public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
 }
